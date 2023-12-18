806a66d572558cff970ca9e8c51e0d7159b87f54
surendermeena <106897366+surendermeena@users.noreply.github.com>
2023-12-18 10:28:19 +0000

    const declared for all elements in HTML

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const inputSlider = document.querySelector('[data-lengthSlider]');
+// to fetch custom attribute from HTML Use --> [] <-- data-lengthSlider;
+
+const lengthDisplay = document.querySelector('[data-lengthNumber]');
+
+const passwordDisplay = document.querySelector("[data-passwordDisplay]")
+const copyBtn = document.querySelector("[data-copyMsg]")
+const copyMsg = document.querySelector("[data-copyMsg]")
+const uppercaseCheck = document.querySelector('#uppercase')
+const lowercaseCheck = document.querySelector('#lowercase')
+const numbersCheck = document.querySelector('#numbers')
+const symbolsCheck = document.querySelector('#symbols')
+const indicator = document.querySelector('[data-indicator]')
+const generateBtn = document.querySelector('.generate-button')
+const allCheckBox = document.querySelector('input[type=checkbox]')
+
+
