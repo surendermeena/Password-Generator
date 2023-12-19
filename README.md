43233d0241c982981cfb0214768d0a61228ceeac
Surender Meena <surender1697@gmail.com>
2023-12-19 15:00:04 +0530

    random integer generator function created for checks

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Password-Generator
+
 Generate password as per your requirement.
 
-Completed - 41:29
+Completed - 41:29 --->18-12-23
+Completed - 01:26:00 --->19-12-23
--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-
-const inputSlider = document.querySelector('[data-lengthSlider]');
+const inputSlider = document.querySelector("[data-lengthSlider]");
 // to fetch custom attribute from HTML Use --> [] <-- data-lengthSlider;
 
-const lengthDisplay = document.querySelector('[data-lengthNumber]');
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
 
-const passwordDisplay = document.querySelector("[data-passwordDisplay]")
-const copyBtn = document.querySelector("[data-copyMsg]")
-const copyMsg = document.querySelector("[data-copyMsg]")
-const uppercaseCheck = document.querySelector('#uppercase')
-const lowercaseCheck = document.querySelector('#lowercase')
-const numbersCheck = document.querySelector('#numbers')
-const symbolsCheck = document.querySelector('#symbols')
-const indicator = document.querySelector('[data-indicator]')
-const generateBtn = document.querySelector('.generate-button')
-const allCheckBox = document.querySelector('input[type=checkbox]')
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copyMsg]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generate-button");
+const allCheckBox = document.querySelector("input[type=checkbox]");
+const symbolString = "~!@#$%^&*()_+={}[]:";
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+// set strength color - Gray;
+
+handleSlider();
+
+// sets password length according to slider
+function handleSlider() {
+  inputSlider.value = passwordLength;
+  lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color) {
+  indicator.style.backgroundColor = color;
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomNumber() {
+  return getRandomInteger(0, 10);
+}
+
+function generateLowerCase() {
+  return String.fromCharCode(getRandomInteger(97, 124));
+}
+
+function generateUpperCase() {
+  return String.fromCharCode(getRandomInteger(65, 91));
+}
+
+function generateSymbol() {
+    const randNum = getRandomInteger(0, (symbolString.length + 1));
+    
+  
+    return symbolString.charAt[randNum]
+}
+
+// not working right now may be reviewed in Next session.--->
+console.log(generateSymbol());
 
 
+function calcStrength(){
+    
+}
